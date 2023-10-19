eef33da7d836bc901d9c4e9326f83a569c5ed712
Saurabh Tarun Mishra <96341901+starun2202@users.noreply.github.com>
2023-10-19 17:27:49 +0530

    Add files via upload

---
+++ FirstSQLGithub.sql
@@ -0,0 +1,41 @@
+Table: Students
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| student_name  | varchar |
++---------------+---------+
+student_id is the primary key (column with unique values) for this table.
+Each row of this table contains the ID and the name of one student in the school.
+ 
+
+Table: Subjects
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| subject_name | varchar |
++--------------+---------+
+subject_name is the primary key (column with unique values) for this table.
+Each row of this table contains the name of one subject in the school.
+ 
+
+Table: Examinations
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| student_id   | int     |
+| subject_name | varchar |
++--------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+Each student from the Students table takes every course from the Subjects table.
+Each row of this table indicates that a student with ID student_id attended the exam of subject_name.
+
+## Solution
+
+Select St.student_id, st.student_name, 
+Ex.subject_name, count(Ex.subject_name) as attended_exams
+from Students as St
+Inner Join Examinations as Ex on St.student_id = Ex.student_id
